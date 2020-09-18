684123ec78f9839ef305112b37b9c28892ec42f5
Yurii Bezpalko <yurii.bezpalko@currencycloud.com>
2020-09-19 01:52:42 +0300

    Added stable waiter method

--- LoremIpsumProject/Tests/BaseTest.cs
+++ LoremIpsumProject/Tests/BaseTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -10,36 +9,34 @@ namespace LoremIpsumProject.Tests
 {
     public class BaseTest
     {
-        IWebDriver driver;
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
         private readonly string test_url = "https://lipsum.com/";
 
         [FindsBy(How = How.XPath, Using = "//a[@class='ru']")]
-        private readonly IWebElement russian_button;
+        private IWebElement russian_button;
 
         [FindsBy(How = How.CssSelector, Using = "#Panes > div:nth-child(1) > p")]
-        private readonly IWebElement first_element_text;
+        private IWebElement first_element_text;
 
         [FindsBy(How = How.XPath, Using = "//input[@id='generate']")]
-        private readonly IWebElement generate_button;
+        private IWebElement generate_button;
 
         [FindsBy(How = How.CssSelector, Using = "#lipsum > p:nth-child(1)")]
-        private readonly IWebElement first_paragraph;
+        private IWebElement first_paragraph;
 
         [FindsBy(How = How.XPath, Using = "//label[@for='words']")]
-        private readonly IWebElement words;
+        private IWebElement words;
 
         [FindsBy(How = How.XPath, Using = "//input[@type='text']")]
-        private readonly IWebElement number_field;
+        private IWebElement number_field;
 
-        [FindsBy(How = How.XPath, Using = "//div[@id='lipsum']")]
-        private readonly IWebElement number_of_words;
+        [FindsBy(How = How.Id, Using = "lipsum")]
+        private IWebElement number_of_words;
 
         private readonly long timeout = 3000;
 
-        public void ImplicitWait()
-        {
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(timeout);
-        }
 
         public void WaitForPageLoadComplete()
         {
@@ -47,32 +44,36 @@ namespace LoremIpsumProject.Tests
             wait.Until(driver1 => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
         }
 
-        public void WaitVisibilityOfElements(IWebElement element)
+        public void WaitForElement(IWebElement element, WebDriverWait wait, int timeout = 2)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            do
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            wait.IgnoreExceptionTypes(typeof(StaleElementReferenceException));
+            wait.Until<bool>(driver =>
             {
-                if (element.Displayed)
+                try
                 {
-                    return;
+                    return element.Displayed;
                 }
-            } while (stopwatch.ElapsedMilliseconds < timeout);
-            throw new Exception("element not found");
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
         }
 
         [SetUp]
         public void StartBrowser()
         {
             driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(test_url);
             PageFactory.InitElements(driver, this);
         }
 
         [Test]
         public void CheckTheWordAppearsCorrectly()
         {
-            driver.Navigate().GoToUrl(test_url);
             russian_button.Click();
             WaitForPageLoadComplete();
             Assert.IsTrue(first_element_text.Text.Contains("рыба"));
@@ -81,26 +82,25 @@ namespace LoremIpsumProject.Tests
         [Test]
         public void CheckTheFirstParagraphStartsCorrectly()
         {
-            driver.Navigate().GoToUrl(test_url);
             generate_button.Click();
             WaitForPageLoadComplete();
             Assert.IsTrue(first_paragraph.Text.Contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit"));
         }
 
-        [TestCase(10)]
-        [TestCase (-1)]
+        [TestCase(20)]
+        [TestCase(-1)]
         [TestCase(0)]
         [TestCase(5)]
-        [TestCase(20)]
+        [TestCase(10)]
         public void CheckThatGeneratedSizeCorrect(int value)
         {
-            driver.Navigate().GoToUrl(test_url);
             words.Click();
             number_field.Clear();
             number_field.SendKeys(Convert.ToString(value));
             generate_button.Click();
-            WaitVisibilityOfElements(number_of_words);
-            int length = number_of_words.Text.Trim(new char[] { ',', '.' }).Split(new char[] { ' ' }).Length;
+            WaitForElement(number_of_words, wait);
+            string for_test = number_of_words.Text;
+            int length = number_of_words.Text.Trim(new char[] { ',', '.' }).Split(" ").Length;
             Assert.AreEqual(value, length);
         }
 
